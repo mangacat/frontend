bb645e3b587fad03261ff0b0f783c5bb843782a5
daszgfz <dasZGFz@protonmail.com>
2019-06-18 23:26:01 -0700

    feat: set lang before sapper starts

--- src/client.js
+++ src/client.js
@@ -1,10 +1,27 @@
+import './css/tailwind.pcss'
 import * as sapper from '@sapper/app'
 import * as Sentry from '@sentry/browser'
-import './css/tailwind.pcss'
+import { locale, dictionary, getClientLocale } from 'svelte-i18n/dist/i18n.mjs'
 
 const dev = process.env.NODE_ENV === 'development'
 if (!dev && process.env.SENTRY_DSN) Sentry.init({ dsn: process.env.SENTRY_DSN })
 
-sapper.start({
-	target: document.querySelector('#app')
+const langs = ['en-US']
+
+let lang = getClientLocale({
+	fallback: 'en-US',
+	navigator: true
 })
+if (!langs.includes(lang)) lang = langs.includes(lang.split('-')[0]) ? lang.split('-')[0] : 'en-US'
+
+fetch(`/i18n/${lang}.json`)
+	.then(res => res.json())
+	.then(res => {
+		const dict = {}
+		dict[lang] = res
+		dictionary.set(dict)
+		locale.set(lang)
+		sapper.start({
+			target: document.querySelector('#app')
+		})
+	})
