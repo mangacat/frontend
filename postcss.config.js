42e84e100e0e000b030d5f04ac704d3319b48cb2
daszgfz <dasZGFz@protonmail.com>
2019-06-19 23:55:38 -0700

    chore(deps): remove postcss-for

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,6 @@ module.exports = {
 	plugins: [
 		require('tailwindcss')('./tailwind.config.js'),
 		require('postcss-nested'),
-		require('postcss-for'),
 		require('css-prefers-color-scheme/postcss'),
 		require('postcss-preset-env')({ stage: 1 }),
 		!dev && require('@fullhuman/postcss-purgecss')({
