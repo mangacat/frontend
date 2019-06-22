8de23c0857bc1af1df38325768ad3faf9078071a
daszgfz <dasZGFz@protonmail.com>
2019-06-22 03:42:23 -0700

    fix: generate wallpaper once per page

--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -5,7 +5,7 @@
 
 <div class="min-h-screen">
     <div class="w-full max-w-sm lg:max-w-2xl lg:flex mx-auto mt-24 shadow-md rounded overflow-hidden mx-2">
-        <div class="hidden lg:block w-1/2 bg-cover bg-center" style="{width > 1024 ? `background-image: url(${cdn(wallpaper(), { w: 1000 })});` : '' }" />
+        <div class="hidden lg:block w-1/2 bg-cover bg-center" style="{width >= 1024 ? `background-image: url(${cdn(img, { w: 1000 })});` : '' }" />
         <div class="lg:w-1/2 bg-white dark:bg-gray-700 px-8 pt-6 pb-8">
             <form bind:this={form_element} on:submit|preventDefault={submit}>
                 <div class="mb-4">
@@ -112,6 +112,7 @@
     let errors = {}
     let submitting = false
     let width
+    const img = wallpaper()
     
     const form_rules = {
     	username(val) {
