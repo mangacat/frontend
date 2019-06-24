01f6c97e4a297519fb4099917c72fcee6fad74c3
daszgfz <dasZGFz@protonmail.com>
2019-06-24 02:32:31 -0700

    fix: add hash to css file

---
+++ scripts/hashCssFile.js
@@ -0,0 +1,18 @@
+const { createHash } = require('crypto')
+const { readFile, writeFile, unlink } = require('fs')
+
+let digest = ''
+const hash = createHash('sha256')
+const catchErr = err => { if (err) throw err }
+
+readFile('./static/css/app.css', (_, data) => {
+	hash.update(data)
+	digest = hash.digest('hex').substring(0, 8)
+	writeFile(`./static/css/app.${digest}.css`, data, catchErr)
+
+	readFile('./__sapper__/build/template.html', 'utf8', (_, data) => {
+		writeFile(`./__sapper__/build/template.html`, data.replace('app.css', `app.${digest}.css`), catchErr)
+	})
+
+	unlink('./static/css/app.css', catchErr)
+})
