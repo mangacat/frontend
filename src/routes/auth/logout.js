39bc6a576181835dfeb6d7c590b5fc7957b093c6
daszgfz <dasZGFz@protonmail.com>
2019-06-26 14:52:16 -0700

    fix: try to fix redis sessions not deleting

--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -1,6 +1,10 @@
-export function post(req, res) {
-	delete req.session.user
-	
-	res.setHeader('Content-Type', 'application/json')
-	res.end(JSON.stringify({ ok: true }))
+export function post(req, res, next) {
+	delete req.session.destroy(err => {
+		if (err) {
+			console.log(err) // eslint-disable-line no-console
+			return next(err)
+		}
+		res.setHeader('Content-Type', 'application/json')
+		res.end(JSON.stringify({ ok: true }))
+	})
 }
