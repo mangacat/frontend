4fb5ff5b9b348f1a840ccc4f5e5fc42caa3f299c
daszgfz <dasZGFz@protonmail.com>
2019-07-06 10:21:48 -0700

    feat: add loading state to front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,29 +3,34 @@
 </svelte:head>
 
 <div class="min-h-screen max-w-xl mx-auto pt-8 px-4">
-    {#each chapters as chapter, index}
-        {#if index === 0 || chapters[index - 1].series.id !== chapter.series.id}
-            <a href="/series/{chapter.series.id}/{slugify(chapter.series.name)}" class="hover:underline block py-1 border-b border-gray-500 dark:border-gray-600">
-                {chapter.series.name}
-            </a>
-        {/if}
-        <Chapter {chapter} />
-    {/each}
+    {#if loading}
+        {#each [...Array(10).keys()] as i}
+            <LoadingPulse class="{['max-w-lg', 'w-64', 'w-48'][Math.round(Math.random() * 3)]} h-8 bg-gray-200 dark:bg-gray-700 rounded mb-4" />
+        {/each}
+    {:else}
+        {#each chapters as chapter, index}
+            {#if index === 0 || chapters[index - 1].series.id !== chapter.series.id}
+                <a href="/series/{chapter.series.id}/{slugify(chapter.series.name)}" class="hover:underline block py-1 border-b border-gray-500 dark:border-gray-600">
+                    {chapter.series.name}
+                </a>
+            {/if}
+            <Chapter {chapter} />
+        {/each}
+    {/if}
 </div>
 
-<script context="module">
-    import * as api from 'api.js'
-
-    export async function preload() {
-    	const chapters = await api.get(`series_chapters?sortby=TimeUploaded&order=desc&limit=25`)
-        
-    	return { chapters }
-    }
-</script>
-
 <script>
+    import * as api from 'api.js'
+    import { onMount } from 'svelte'
     import { slugify } from 'utils.js'
     import Chapter from 'components/Chapter.svelte'
+    import LoadingPulse from 'components/LoadingPulse.svelte'
 
-    export let chapters
+    let chapters = []
+    let loading = true
+
+    onMount(async () => {
+    	chapters = await api.get(`series_chapters?sortby=TimeUploaded&order=desc&limit=25`)
+    	loading = false
+    })
 </script>
