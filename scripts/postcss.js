b3da6196b9dee124732e87954123c94aeb66b349
daszgfz <dasZGFz@protonmail.com>
2019-07-13 01:12:44 -0700

    refactor: remove async function

--- scripts/postcss.js
+++ scripts/postcss.js
@@ -5,21 +5,23 @@ import { readFileSync, writeFileSync, mkdirSync, existsSync } from 'fs'
 
 const { plugins } = config
 
-async function process() {
+function process() {
 	const css = readFileSync('./src/css/tailwind.pcss', { encoding: 'utf8' })
-	const result = await postcss(plugins).process(css, { 
-		from: './src/css/tailwind.pcss', 
-		to: './static/css/app.css', 
-		map: false 
-	})
+
+	return postcss(plugins)
+		.process(css, {
+			from: './src/css/tailwind.pcss',
+			to: './static/css/app.css',
+			map: false
+		}).then(result => {
+			if (!existsSync('./static/css/')) mkdirSync('./static/css/')
 	
-	if (!existsSync('./static/css/')) mkdirSync('./static/css/')
+			const hash = createHash('md5').update(result.css).digest('hex').substring(0, 8)
+			writeFileSync(`./static/css/app.${hash}.css`, result.css)
 	
-	const hash = createHash('md5').update(result.css).digest('hex').substring(0, 8)
-	writeFileSync(`./static/css/app.${hash}.css`, result.css)
-	
-	const file = readFileSync('./src/template.html', { encoding: 'utf8' })
-	writeFileSync('./src/template.html', file.replace(new RegExp('/css/app.+?css'), `/css/app.${hash}.css`))
+			const file = readFileSync('./src/template.html', { encoding: 'utf8' })
+			writeFileSync('./src/template.html', file.replace(new RegExp('/css/app.+?css'), `/css/app.${hash}.css`))
+		})
 }
 
 console.time('Built CSS in')
