80f8f154466ddef6fee2de36ebbe2fa4d2e395e5
daszgfz <dasZGFz@protonmail.com>
2019-07-22 08:15:03 -0700

    chore: add svelte.config.js for vscode

--- rollup.config.js
+++ rollup.config.js
@@ -7,19 +7,12 @@ import config from 'sapper/config/rollup.js'
 import { terser } from 'rollup-plugin-terser'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
-import sveltePreprocess from 'svelte-preprocess'
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
-const { plugins } = require('./postcss.config.js')
-
-const preprocess = sveltePreprocess({
-	transformers: {
-		postcss: { plugins }
-	}
-})
+const { preprocess } = require('./svelte.config.js')
 
 const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' &&  /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning)
 const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/')
