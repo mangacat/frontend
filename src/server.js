951f8f795af51f4cbd7124abc21b293cabd07a07
daszgfz <dasZGFz@protonmail.com>
2019-08-12 23:36:36 -0700

    chore: simplify dev environment

--- src/server.js
+++ src/server.js
@@ -10,37 +10,52 @@ let store = undefined
 
 const { PORT, NODE_ENV, SENTRY_DSN, REDIS_HOST, REDIS_PORT, REDIS_PASS, SESSION_SECRET } = process.env
 const dev = NODE_ENV === 'development'
+const app = polka()
 
-if (!dev && SENTRY_DSN) Sentry.init({ dsn: SENTRY_DSN })
-if (!dev && REDIS_HOST) {
-	const RedisStore = require('connect-redis')(session)
-	store = new RedisStore({
-		host: REDIS_HOST,
-		port: REDIS_PORT,
-		pass: REDIS_PASS
-	})
+if (dev) {
+	app
+		.use(
+			sirv('static', { dev }),
+			sapper.middleware({
+				session: req => ({
+					user: req.session && req.session.user
+				})
+			})
+		)
+		.listen(PORT, err => {
+			if (err) console.log('error', err) // eslint-disable-line no-console
+		})
+} else {
+	if (SENTRY_DSN) Sentry.init({ dsn: SENTRY_DSN })
+	if (REDIS_HOST) {
+		const RedisStore = require('connect-redis')(session)
+		store = new RedisStore({
+			host: REDIS_HOST,
+			port: REDIS_PORT,
+			pass: REDIS_PASS
+		})
+	}
+
+	app
+		.use(bodyParser.json())
+		.use(session({
+			store,
+			secret: SESSION_SECRET,
+			cookie: {
+				maxAge: 86400000
+			},
+			resave: false,
+			saveUninitialized: false
+		}))
+		.use(
+			shrinkRayCurrent(),
+			sirv('static', { dev, etag: true, maxAge: 31536000, immutable: true }),
+			sapper.middleware({
+				session: req => ({
+					user: req.session && req.session.user
+				})
+			})
+		)
 }
 
-polka()
-	.use(bodyParser.json())
-	.use(session({
-		store,
-		secret: SESSION_SECRET,
-		cookie: {
-			maxAge: 86400000
-		},
-		resave: false,
-		saveUninitialized: false
-	}))
-	.use(
-		shrinkRayCurrent(),
-		sirv('static', { dev, etag: true, maxAge: 31536000, immutable: true }),
-		sapper.middleware({
-			session: req => ({
-				user: req.session && req.session.user
-			})
-		})
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err) // eslint-disable-line no-console
-	})
+export { app }
