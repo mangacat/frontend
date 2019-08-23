bc7762ccb55cb265a6e00bbc626e6dff21599465
daszgfz <dasZGFz@protonmail.com>
2019-08-23 01:17:30 -0700

    fix: fix postcss script

--- scripts/postcss.js
+++ scripts/postcss.js
@@ -13,12 +13,17 @@ exports.css = function () {
 			map: false
 		})
 		.then(({ css }) => {
-			existsSync('./static/css/') || mkdirSync('./static/css/')
+			!existsSync('./static/css/') && mkdirSync('./static/css/')
 	
 			const hash = createHash('md5').update(css).digest('hex').substring(0, 8)
 			writeFileSync(`./static/css/app.${hash}.css`, css)
 	
 			const file = readFileSync('./src/template.html', { encoding: 'utf8' })
 			writeFileSync('./src/template.html', file.replace(new RegExp('/css/app.+?css'), `/css/app.${hash}.css`))
+
+			if (existsSync('./__sapper__/build/template.html')) {
+				const file2 = readFileSync('./__sapper__/build/template.html', { encoding: 'utf8' })
+				writeFileSync('./__sapper__/build/template.html', file2.replace(new RegExp('/css/app.+?css'), `/css/app.${hash}.css`))
+			}
 		})
 }
