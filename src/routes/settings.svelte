3bce40879b7039c9af831e1d9dcfe6e2da2d5bfc
daszgfz <dasZGFz@protonmail.com>
2019-09-07 00:29:53 -0400

    refactor: Button components

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -58,8 +58,10 @@
                         <img alt="mugshot image" class="w-16 h-16 object-cover object-center rounded-full cursor-pointer mr-4" src="{mugshotSrc}" />
                     {/if}
                     <div class="flex items-center relative cursor-pointer">
-                        <input bind:files={mugshotFileInput} type="file" accept="image/jpeg,image/png,image/gif" class="opacity-0 absolute top-0 left-0 h-0 w-0">
-                        <div class="bg-gray-300 hover:bg-gray-200 font-bold text-sm py-1 px-3 rounded">Change</div>
+                        <input bind:this={mugshotFileInputElem} bind:files={mugshotFileInput} id="mugshot" type="file" accept="image/jpeg,image/png,image/gif" class="opacity-0 absolute top-0 left-0 h-0 w-0">
+                        <PrimaryButton on:click={() => {mugshotFileInputElem.click()}} class="dark:bg-gray-700">
+                            Upload
+                        </PrimaryButton>
                     </div>
                 </div>
             </label>
@@ -70,8 +72,10 @@
                         <img alt="cover image" class="w-64 h-32 object-cover object-center rounded-lg cursor-pointer mr-4" src="{coverSrc}" />
                     {/if}
                     <div class="flex items-center relative cursor-pointer">
-                        <input bind:files={coverFileInput} type="file" accept="image/jpeg,image/png,image/gif" class="opacity-0 absolute top-0 left-0 h-0 w-0">
-                        <div class="bg-gray-300 hover:bg-gray-200 font-bold text-sm py-1 px-3 rounded">Change</div>
+                        <input bind:this={coverFileInputElem} bind:files={coverFileInput} id="cover" type="file" accept="image/jpeg,image/png,image/gif" class="opacity-0 absolute top-0 left-0 h-0 w-0">
+                        <PrimaryButton on:click={() => {coverFileInputElem.click()}} class="dark:bg-gray-700">
+                            Upload
+                        </PrimaryButton>
                     </div>
                 </div>
             </label>
@@ -97,7 +101,9 @@
         <div class="py-4 px-5 bg-gray-100 dark:bg-gray-800">
             <label class="block sm:flex my-4">
                 <span class="sm:inline-flex sm:w-1/3 font-bold items-center text-sm justify-end pr-4">Delete account</span>
-                <button class="text-white bg-red-500 py-2 px-4 rounded">Delete account</button>
+                    <PrimaryButton class="dark:bg-gray-700">
+                        Delete account
+                    </PrimaryButton>
             </label>
         </div>
     </div>
@@ -105,8 +111,12 @@
 
 <div class="fixed z-100 bottom-0 inset-x-0 bg-white dark:bg-gray-700 dark:border-gray-900 border-t border-gray-300">
     <div class="max-w-3xl mx-4 md:mx-auto my-4 flex justify-end">
-        <button on:click={() => {goto('/')}} class="bg-gray-300 hover:bg-gray-200 font-semibold py-2 px-4 rounded mr-4" type="button">Cancel</button>
-        <button on:click|once={submit} class="bg-gray-800 hover:bg-gray-700 text-gray-200 font-semibold py-2 px-4 rounded" type="button">Save</button>
+        <SecondaryButton class="mr-4" on:click={() => {goto('/')}}>
+            Cancel
+        </SecondaryButton>
+        <PrimaryButton on:click|once={submit}>
+            Save
+        </PrimaryButton>
     </div>
 </div>
 
@@ -120,6 +130,8 @@
     import { onMount } from 'svelte'
     import { stores, goto } from '@sapper/app'
     import Notifications from 'components/Notifications.svelte'
+    import PrimaryButton from 'components/Buttons/Primary.svelte'
+    import SecondaryButton from 'components/Buttons/Secondary.svelte'
     let notifications
 
     const { session } = stores()
@@ -129,10 +141,12 @@
     let username = $session.user.username
     let description = $session.user.description
     let mugshotSrc = 'https://github.com/daszgfz.png'
+    let mugshotFileInputElem
     let mugshotFileInput
     let mugshotFile
     let coverFile
     let coverFileInput
+    let coverFileInputElem
     let coverSrc = 'https://w.wallhaven.cc/full/4v/wallhaven-4v7zrm.png'
     let oldPassword
     let newPassword
